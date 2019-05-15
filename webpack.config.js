38d4b03293cafc3a667854f4cbc3141948c02126
gondr99 <gondr99@gmail.com>
2019-05-15 19:36:56 +0900

    webpack오류 수정

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,31 @@ const path = require('path');
 const MiniCssExtractor = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry:['./js/App.js'],
-    output:{
-        path:path.resolve(__dirname, "public"),
-        filename:'app.js'
+    entry: ['./js/App.js'],
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: 'app.js'
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.css$/,
-                use:[MiniCssExtractor, 'css-loader']
+                test: /\.css$/,
+                use: [
+                    {
+                        loader:MiniCssExtractor.loader,
+                        options:{
+                            hmr:process.env.NODE_ENV === 'development'
+                        }
+                    },
+                    'css-loader']
             }
         ]
     },
-    plugins:[
-        new MiniCssExtractor({filename:'style.css'})
-    ]
+    plugins: [
+        new MiniCssExtractor({ filename: 'style.css' })
+    ],
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.json', '.jsx', '.css'],
+    },
 }
